7371f8ec10b399dbcb18a9113b6c45881b97816f
100591184-png <100591184@alumnos.uc3m.es>
2026-03-25 00:33:48 +0100

    Add empty register_project method and potentially needed imports - all tests failing

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,4 +1,10 @@
 """Module """
+import json
+import hashlib
+from datetime import datetime
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
+from uc3m_consulting.enterprise_project import EnterpriseProject
+
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
@@ -10,3 +16,9 @@ class EnterpriseManager:
         """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
         OR FALSE IN OTHER CASE"""
         return True
+
+    def register_project(self, company_cif: str, project_achronym: str,
+                         project_description: str, department: str,
+                         date: str, budget: float):
+        """Registers a new project"""
+        pass
